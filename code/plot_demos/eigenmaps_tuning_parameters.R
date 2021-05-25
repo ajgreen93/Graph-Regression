ac4076e12f59bf8ff842d55711e49f6aa6ac476c
Alden Green <ajgreen@andrew.cmu.edu>
2021-05-25 10:11:56 -0400

    Changed script for plotting tuning parameters.

--- code/plot_demos/eigenmaps_tuning_parameters.R
+++ code/plot_demos/eigenmaps_tuning_parameters.R
@@ -6,15 +6,17 @@ library(ggplot2)
 library(gridExtra)
 library(viridis)
 source("plot_methods.R")
+source("misc.R")
 
 # User entered information.
-data_directory <- "data/laplacian_eigenmaps/eigenmaps_parameters_2s"
-plot_directory <- "plots/laplacian_eigenmaps/eigenmaps_parameters_2s" # Please change this to whichever directory you prefer.
+data_directory <- "data/laplacian_eigenmaps/tuning/20210525094744"
+plot_directory <- "plots/laplacian_eigenmaps/tuning" # Please change this to whichever directory you prefer.
 if(!exists(plot_directory)){dir.create(plot_directory)}
+plot_n <- 1000                                       # Which sample size would you like to use?
 
 # Load data
 load(file.path(data_directory,"configs.R"))
-d <- configs$d; ns <- configs$ns; methods <- configs$methods
+d <- configs$d; s <- configs$s; ns <- configs$ns; methods <- configs$methods
 load(file.path(data_directory,"best_fits_by_method.R"))
 load(file.path(data_directory,"thetas.R"))
 load(file.path(data_directory,"mse.R"))
@@ -22,48 +24,57 @@ load(file.path(data_directory,"Xs.R"))
 load(file.path(data_directory,"f0s.R"))
 load(file.path(data_directory,"Ys.R"))
 
+stopifnot(plot_n %in% ns)
+mse <- mse[plot_n %in% ns]
+methods <- methods[plot_n %in% ns]
+thetas <- thetas[plot_n %in% ns]
+
 # Plotting parameters for all plots
 title <- paste0("d = ",d,", s = ",s,".")
 
 ## Plot 1: Mean squared error as a function of K.
 
-# Parameters
-alg_indx <- which(names(configs$methods) == "laplacian_eigenmaps")
-n_indx <- length(ns)
-all_rs <- unique(thetas[[n_indx]][[alg_indx]]$r)
-rs <- all_rs[round(seq(1,length(all_rs),length.out = 9))]
-Ks <- unique(thetas[[n_indx]][[alg_indx]][,"K"])
+# Find best radius
+best_r <- find_best_radius(mse,thetas)
+alg_indx <- sapply(thetas[[1]],FUN = function(theta){"K" %in% names(theta)}) %>% which()
+Ks <- sapply(thetas[[1]][alg_indx],FUN = function(theta){unique(theta$K)})
 
-# Data for plotting
-plot_mse <- mse[[n_indx]][[alg_indx]][which(thetas[[n_indx]][[alg_indx]]$r %in% rs),] %>%
-  rowMeans()
-plot_rs <- thetas[[n_indx]][[alg_indx]][which(thetas[[n_indx]][[alg_indx]]$r %in% rs),"r"]
-plot_Ks <- thetas[[n_indx]][[alg_indx]][which(thetas[[n_indx]][[alg_indx]]$r %in% rs),"K"]
-plot_mat <- cbind(plot_mse,plot_rs,plot_Ks); colnames(plot_mat) <- c("mse","r","K")
+# Parameters
+plot_mse <- matrix(nrow = nrow(Ks),ncol = ncol(Ks))
+for(ii in alg_indx)
+{
+  if(is.na(best_r[ii]))
+  {
+    plot_mse[,ii] <- rowMeans(mse[[1]][[ii]])
+  } else{
+    best_r_row <- which(thetas[[1]][[ii]]$r %in% best_r)
+    plot_mse[,ii] <- rowMeans(mse[[1]][[ii]][best_r_row,])
+  }
+}
 
 # Plotting parameters
 xlims <- c(min(Ks),max(Ks))
 ylims <- c(min(plot_mse),max(plot_mse))
-cols <- viridis(length(rs))
+cols <- c("red","blue","green")
+stopifnot(ncol(plot_mse) <= 3)
 
 pdf(file.path(plot_directory,"mse_by_number_of_eigenvectors.pdf"))
 par(mar = c(5.1,6,4.1,4.1)) # Prevent the left hand side from getting cut off.
 # shadow plot
-plot(x = plot_mat[,"K"], xlim = xlims, ylim = ylims, xlab = "Number of eigenvectors", ylab = "Mean Squared Error", 
+plot(x = Ks[,1], xlim = xlims, ylim = ylims, xlab = "Number of eigenvectors", ylab = "Mean Squared Error", 
      main = title,
      cex.main = 2.5, cex.lab = 2.5, cex.axis = 2, type = "n")
 
 # add points and lines
-for(jj in 1:length(rs))
+for(jj in 1:ncol(plot_mse))
 {
-  plot_indx <- which(plot_mat[,"r"] == rs[jj])
-  points(x = plot_mat[plot_indx,"K"], y =  plot_mat[plot_indx,"mse"], col = cols[jj], pch = 20)
-  lines(x = plot_mat[plot_indx,"K"], y =  plot_mat[plot_indx,"mse"], col = cols[jj], lwd = 1.5)
+  points(x = Ks[,jj], y =  plot_mse[,jj], col = cols[jj], pch = 20)
+  lines(x = Ks[,jj], y =  plot_mse[,jj], col = cols[jj], lwd = 1.5)
 }
 grid(lwd = 2)
 dev.off()
 
-## Plot 1: Mean squared error as a function of K.
+## Plot 2: Mean squared error as a function of radius.
 
 # Parameters
 alg_indx <- which(names(configs$methods) == "laplacian_eigenmaps")
